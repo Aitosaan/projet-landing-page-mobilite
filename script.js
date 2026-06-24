4b23f01bdf49a83a3031b18096d512a355b63dd2
Aitosaan <sanaito@hotmail.fr>
2026-06-24 14:39:01 +0200

    commitQuatorzeTrente

--- script.js
+++ script.js
@@ -7,22 +7,36 @@ let sectionBenefices = document.getElementById("benefices");
 let sectionPreuve = document.getElementById("preuve");
 let sectionPartenaires = document.getElementById("partenaires");
 let sectionEngagement = document.getElementById("engagement");
-let footer = document.getElementsByTagName("footer");
+let footer = document.getElementsByTagName("footer")[0];
 
 let listeAncres = [sectionHero, sectionProblematique, sectionSolution, sectionBenefices, sectionPreuve, sectionPartenaires, sectionEngagement, footer];
-
-
+let index = 0;
 
 document.addEventListener("keydown", (e) => {
     // écouter appui flèche du bas
-    if (e.which === 40) {
-    console.log("downArrowKey was pressed")
-    // naviguer jusqu'à section suivante (+1 dans la liste)
+  // if (e.which === 40) {
+  if (e.key === "ArrowDown") {
+    
+    if (index < listeAncres.length -1) {
+          e.preventDefault();
+
+      console.log("downArrowKey was pressed")
+      // naviguer jusqu'à section suivante (+1 dans la liste)
+      index++;
+      listeAncres[index].scrollIntoView({behavior: 'smooth' });
+    }
   }
     // écouter appui flèche du haut
-    else if (e.which === 38) {
-    console.log("upArrowKey was pressed")
-    // naviguer jusqu'à section précédente (-1 dans la liste)
+  // else if (e.which === 38) {
+  else if (e.key === "ArrowUp") {
+    e.preventDefault();
+
+    if (index > 0) {
+      console.log("upArrowKey was pressed")
+      // naviguer jusqu'à section précédente (-1 dans la liste)
+      index--;
+      listeAncres[index].scrollIntoView({behavior: 'smooth' });
+    }
   }
 });
 
